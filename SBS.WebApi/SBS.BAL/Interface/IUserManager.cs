186c9a24d34f0b29e8e67fdf4618bdd634ca2d84
Deep18-03 <deepmshah113@gmail.com>
2021-02-15 23:26:24 +0530

    .net assignment

---
+++ SBS.WebApi/SBS.BAL/Interface/IUserManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SBS.Models;
+
+
+
+namespace SBS.BAL.Interface
+{
+    public interface IUserManager
+    {
+        List<string> AuthenticateCustomer(Customer model);
+        List<string> CreateCustomer(Customer model);
+    }
+}
